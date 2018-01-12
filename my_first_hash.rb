35da68ed7008f14d77eaacfeb529754606014961
Zachary Wolner <zachary.wolner@gmail.com>
2018-01-12 19:47:48 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -32,11 +32,11 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 
-
+  shipping_manifest["muskets"] = 2
 
   # add 4 gun powder to the shipping_manifest hash below
 
-
+  shipping_manifest["gun powder"] = 4
 
   # return the shipping_manifest hash below
 
